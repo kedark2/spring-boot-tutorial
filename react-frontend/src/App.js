74830de59a1606f1f7e8c379493d52cf28506966
Kedar Kanel <kedar.kanel@gmail.com>
2020-10-22 09:20:02 +0300

    added ListEmployeeComponent

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ListEmployeeComponent from './components/ListEmployeeComponent'
 
 function App() {
   return (
     <div className="App">
       <h1>Lets start java spring boot</h1>
+      <ListEmployeeComponent />
     </div>
   );
 }
