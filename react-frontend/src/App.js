9a07b0d1e6cfa7147e0dac03e0f8d1b5cd511578
Kedar Kanel <kedar.kanel@gmail.com>
2020-10-22 09:57:04 +0300

    added routing functionality

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 import HeaderComponent from './components/HeaderComponent'
 import FooterComponent from './components/FooterComponent'
 
 function App() {
   return (
+
     <div className="App">
-      <HeaderComponent />
-      <div className="container">
-        <ListEmployeeComponent />
-      </div>
-      <FooterComponent />
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Switch>
+            <Route path="/" component={ListEmployeeComponent}>
+            </Route>
+            <Route path="/employees" component={ListEmployeeComponent}>
+            </Route>
+            <ListEmployeeComponent />
+          </Switch>
+        </div>
+        <FooterComponent />
+      </Router>
     </div>
   );
 }
